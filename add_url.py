70ca5e2bce3308eb4b217663e37f2ff432cf344b
other_wolf <vpe304@gmail.com>
2025-02-06 13:01:03 +0300

    add more sites

--- add_url.py
+++ add_url.py
@@ -19,13 +19,13 @@ def add_to_hosts(domain):
         # Проверяем, есть ли уже домен в файле
         with open(HOSTS_FILE_PATH, "r") as hosts_file:
             lines = hosts_file.readlines()
-            if any(domain in line for line in lines):
+            if any(f' {domain}' in line for line in lines):
                 print(f"Домен {domain} уже добавлен в файл hosts.")
                 return
 
         # Добавляем домен в файл
         with open(HOSTS_FILE_PATH, "a") as hosts_file:
-            hosts_file.write(f"{IP_ADDRESS}      {domain}\n")
+            hosts_file.write(f"{IP_ADDRESS}     {domain}\n")
         print(f"Домен {domain} успешно добавлен в файл hosts.")
     except PermissionError:
         print("Ошибка: требуется запуск с правами администратора.")
@@ -42,11 +42,15 @@ def main():
             print("Выход из программы.")
             break
 
-        domain = extract_domain(url)
-        if domain:
-            add_to_hosts(domain)
-        else:
-            print("Некорректный URL. Попробуйте снова.")
+        urls = [u.strip() for u in url.split() if u.strip()]
+        for url in urls:
+
+            domain = extract_domain(url)
+            if domain:
+                add_to_hosts(domain)
+            else:
+                print("Некорректный URL. Попробуйте снова.")
+                break
 
 
 if __name__ == "__main__":
